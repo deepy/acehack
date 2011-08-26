83405d6a3d227b411cb74b1985cf14da016e1d05
Alex Smith <ais523@bham.ac.uk>
2011-08-26 14:42:23 +0100

    Fix conflicts over last-modified dates
    Ignore-this: 29b8583674a654737ab5a89b0a54c5ed
    
    darcs-hash:20110826134223-99289-ea01eb05c4d298e2a33d5c01d0a7e89455ff5035

--- src/pickup.c
+++ src/pickup.c
@@ -1,6 +1,6 @@
 /*	SCCS Id: @(#)pickup.c	3.4	2003/07/27	*/
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
-/* Modified 18 Jun 2011 by Alex Smith */
+/* Modified 19 Aug 2011 by Alex Smith */
 /* NetHack may be freely redistributed.  See license for details. */
 
 /*
